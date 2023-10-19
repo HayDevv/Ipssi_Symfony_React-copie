7faefd6835c5c8915f9124a0648914cc864626c5
Sudo-Abdelhay <boutabba.abdelhay@gmail.com>
2023-10-19 11:20:26 +0200

    Mise en forme et fixtures

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import dataFixtures from './dataFixtures'; // Importez vos fixtures
+import React, { useState } from 'react';
+import dataFixtures from './dataFixtures';
 
 const DataTableUser = () => {
     const [data, setData] = useState(dataFixtures);
@@ -11,12 +11,7 @@ const DataTableUser = () => {
     };
 
     const handleUpdate = (id) => {
-        const updatedData = data.map((item) => {
-            if (item.id === id) {
-                return newItem;
-            }
-            return item;
-        });
+        const updatedData = data.map((item) => (item.id === id ? newItem : item));
         setData(updatedData);
         setEditingId(null);
     };
@@ -34,45 +29,121 @@ const DataTableUser = () => {
     const handleAdd = () => {
         if (newItem.nom && newItem.prenom && newItem.age && newItem.email) {
             setData([...data, newItem]);
-            setNewItem({ nom: '', prenom: '', age: '', email: ''});
+            setNewItem({ nom: '', prenom: '', age: '', email: '', commandes: '' });
         } else {
             alert('Veuillez remplir tous les champs.');
         }
     };
 
     return (
-        <div>
-            <table>
+        <div className="container mx-auto mt-8">
+            <h1 className="text-2xl font-bold mb-4">Tableau des Utilisateurs</h1>
+            <table className="table-auto w-full">
                 <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nom</th>
-                        <th>Prénom</th>
-                        <th>Age</th>
-                        <th>Email</th>
-                        <th>Commandes</th>
-                        <th>Actions</th>
+                    <tr className="bg-blue-500 text-white">
+                        <th className="px-4 py-2">ID</th>
+                        <th className="px-4 py-2">Nom</th>
+                        <th className="px-4 py-2">Prénom</th>
+                        <th className="px-4 py-2">Age</th>
+                        <th className="px-4 py-2">Email</th>
+                        <th className="px-4 py-2">Commandes</th>
+                        <th className="px-4 py-2">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {data.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.id}</td>
-                            <td>{editingId === item.id ? <input type="text" value={newItem.nom} onChange={(e) => handleInputChange(e, 'nom')} /> : item.nom}</td>
-                            <td>{editingId === item.id ? <input type="text" value={newItem.prenom} onChange={(e) => handleInputChange(e, 'prenom')} /> : item.prenom}</td>
-                            <td>{editingId === item.id ? <input type="text" value={newItem.age} onChange={(e) => handleInputChange(e, 'age')} /> : item.age}</td>
-                            <td>{editingId === item.id ? <input type="text" value={newItem.email} onChange={(e) => handleInputChange(e, 'email')} /> : item.email}</td>
-                            <td>{editingId === item.id ? <input type="text" value={newItem.commandes} onChange={(e) => handleInputChange(e, 'commandes')} /> : item.commandes}</td>
-                            <td>
+                        <tr key={item.id} className={editingId === item.id ? 'bg-yellow-100' : ''}>
+                            <td className="border px-4 py-2">{item.id}</td>
+                            <td className="border px-4 py-2">
+                                {editingId === item.id ? (
+                                    <input
+                                        type="text"
+                                        value={newItem.nom}
+                                        onChange={(e) => handleInputChange(e, 'nom')}
+                                        className="w-full"
+                                    />
+                                ) : (
+                                    item.nom
+                                )}
+                            </td>
+                            <td className="border px-4 py-2">
+                                {editingId === item.id ? (
+                                    <input
+                                        type="text"
+                                        value={newItem.prenom}
+                                        onChange={(e) => handleInputChange(e, 'prenom')}
+                                        className="w-full"
+                                    />
+                                ) : (
+                                    item.prenom
+                                )}
+                            </td>
+                            <td className="border px-4 py-2">
+                                {editingId === item.id ? (
+                                    <input
+                                        type="text"
+                                        value={newItem.age}
+                                        onChange={(e) => handleInputChange(e, 'age')}
+                                        className="w-full"
+                                    />
+                                ) : (
+                                    item.age
+                                )}
+                            </td>
+                            <td className="border px-4 py-2">
+                                {editingId === item.id ? (
+                                    <input
+                                        type="text"
+                                        value={newItem.email}
+                                        onChange={(e) => handleInputChange(e, 'email')}
+                                        className="w-full"
+                                    />
+                                ) : (
+                                    item.email
+                                )}
+                            </td>
+                            <td className="border px-4 py-2">
+                                {editingId === item.id ? (
+                                    <input
+                                        type="text"
+                                        value={newItem.commandes}
+                                        onChange={(e) => handleInputChange(e, 'commandes')}
+                                        className="w-full"
+                                    />
+                                ) : (
+                                    item.commandes
+                                )}
+                            </td>
+                            <td className="border px-4 py-2">
                                 {editingId === item.id ? (
                                     <>
-                                        <button onClick={() => handleUpdate(item.id)}>Update</button>
-                                        <button onClick={() => setEditingId(null)}>Cancel</button>
+                                        <button
+                                            onClick={() => handleUpdate(item.id)}
+                                            className="bg-green-500 text-white px-2 py-1 mr-2"
+                                        >
+                                            Update
+                                        </button>
+                                        <button
+                                            onClick={() => setEditingId(null)}
+                                            className="bg-red-500 text-white px-2 py-1"
+                                        >
+                                            Cancel
+                                        </button>
                                     </>
                                 ) : (
                                     <>
-                                        <button onClick={() => handleEdit(item.id)}>Edit</button>
-                                        <button onClick={() => handleDelete(item.id)}>Delete</button>
+                                        <button
+                                            onClick={() => handleEdit(item.id)}
+                                            className="bg-blue-500 text-white px-2 py-1 mr-2"
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                            onClick={() => handleDelete(item.id)}
+                                            className="bg-red-500 text-white px-2 py-1"
+                                        >
+                                            Delete
+                                        </button>
                                     </>
                                 )}
                             </td>
@@ -80,19 +151,51 @@ const DataTableUser = () => {
                     ))}
                 </tbody>
             </table>
-            <div>
-                <h2>Add User</h2>
-                <input type="text" placeholder="Nom" value={newItem.nom} onChange={(e) => handleInputChange(e, 'nom')} />
-                <input type="text" placeholder="Prénom" value={newItem.prenom} onChange={(e) => handleInputChange(e, 'prenom')} />
-                <input type="text" placeholder="Age" value={newItem.age} onChange={(e) => handleInputChange(e, 'age')} />
-                <input type="text" placeholder="Email" value={newItem.email} onChange={(e) => handleInputChange(e, 'email')} />
-                <button onClick={handleAdd}>Add</button>
+            <div className="mt-4">
+                <h2 className="text-xl font-bold mb-2">Ajouter un Utilisateur</h2>
+                <div className="grid grid-cols-4 gap-4">
+                    <input
+                        type="text"
+                        placeholder="Nom"
+                        value={newItem.nom}
+                        onChange={(e) => handleInputChange(e, 'nom')}
+                        className="border rounded-md px-3 py-2"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Prénom"
+                        value={newItem.prenom}
+                        onChange={(e) => handleInputChange(e, 'prenom')}
+                        className="border rounded-md px-3 py-2"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Age"
+                        value={newItem.age}
+                        onChange={(e) => handleInputChange(e, 'age')}
+                        className="border rounded-md px-3 py-2"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        value={newItem.email}
+                        onChange={(e) => handleInputChange(e, 'email')}
+                        className="border rounded-md px-3 py-2"
+                    />
+                </div>
+                <button
+                    onClick={handleAdd}
+                    className="bg-blue-500 text-white px-3 py-2 mt-2 rounded-md"
+                >
+                    Ajouter
+                </button>
             </div>
         </div>
     );
 };
 
 export default DataTableUser;
+
 
 
 // const DataTableUser = () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3490dc', // Remplacez la couleur par votre couleur préférée
+        secondary: '#ff9900',
+        // Ajoutez d'autres couleurs personnalisées ici si nécessaire
+      },
+    },
   },
   plugins: [],
 }
